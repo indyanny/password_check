ac86958a810a6c29a02b569bdfb8bbb7d30c4240
indyanny <indy.engenharia@gmail.com>
2024-09-14 22:37:22 -0300

    password_check

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ passwordInput.addEventListener('input',
         let password = event.target.value;
         let strength = Math.min(password.length, 12);
         let degree = strength * 30;
-        let gradientColor = strength <= 4 ? '#ff2c1c':(strength <= 8 ? '#ff9800': '#12ff12');
+        let gradientColor = strength <= 4 ? '#ff2972':(strength <= 8 ? '#fee800': '#04fc43');
         let strengthText = strength <= 4 ? 'Weak': (strength <= 8 ? 'Medium': 'Strong');
 
         passwordStrength.forEach(passwordStrength =>{
